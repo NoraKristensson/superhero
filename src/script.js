04883a03f50439ef25fcc600c2d3fc3483c49a01
“username”Norakristensson <email@email.comkristensson.nora@gmail.com>
2020-06-02 01:18:51 +0200

    one step closer

--- src/script.js
+++ src/script.js
@@ -1,109 +1,29 @@
-// const hero = new Vue({
-//   el: "#hero",
-//   data: {
-//     response: "success",
-//     // results-for: "id",
-//     heroes: [],
-//     searchItem: "";
-//   },
-
-//   mounted: function () {
-//     fetch("https://www.superheroapi.com/api.php/10156900756796046/search", {
-//       method: "get",
-//     })
-//       .then((response) => {
-//         return response.json();
-//       })
-//       // .then((response) => {
-//       //   this.results = response;
-//       // });
-//       .then((response) => {
-//         this.heroes = heroes[2].apperance;
-//         console.log(response);
-//       });
-//   },
-// });
-
-// const demo = new Vue({
-//   el: "#demo",
-//   data: {
-//     parentMsg: "Hello",
-//     items: [{ childMsg: "Foo" }, { childMsg: "Bar" }],
-//   },
-// });
-
-// ---------------------
-
-// const hero = new Vue({
-//   el: "#hero",
-//   data: {
-//     response: "success",
-//     // results-for: "id",
-//     heroes: [],
-//     searchItem: " ",
-//   },
-
-//   mounted: function () {
-//     fetch("https://www.superheroapi.com/api.php/10156900756796046/", {
-//       method: "get",
-//     })
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((heroes) => {
-//         this.hero = heroes;
-//         // ändrade till hero
-//       });
-
-//     // .then((response) => {
-//     //   this.heroes = heroes[2].apperance;
-//     //   console.log(response);
-//     // });
-//   },
-// });
-
-// ------------
-
-const hero = new Vue({
-  el: "#hero",
-  data: function () {
-    return {
-      query: "",
-      results: [],
-    };
+const App = new Vue({
+  el: "#hero ",
+  data: {
+    name: "",
+    superhero: [],
+    modal: false,
   },
 
-  mounted: function () {
-    // fetch(
-    //   `https://www.superheroapi.com/api.php/10156900756796046/search/batman`
-    // )
-    fetch(
-      `https://www.superheroapi.com/api.php/10156900756796046/search/${this.query}`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        this.results = response;
-      });
-    //     .then((response) => {
-    //       this.results = results[2].apperance;
-    //       console.log(response);
-    //     });
+  methods: {
+    getSuperhero() {
+      fetch(
+        `https://www.superheroapi.com/api.php/10156900756796046/search/${this.name}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.superhero = data;
+        });
+    },
+    getMoreInfo() {
+      fetch(
+        `https://www.superheroapi.com/api.php/10156900756796046/search/${this.name}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.superhero = data;
+        });
+    },
   },
-
-  // methods: {
-  //   getSuperhero() {
-  //     fetch(`https://superheroapi.com/api/10156900756796046/search/${query}`)
-  //       .then((response) => {
-  //         return response.json();
-  //       })
-  //       .then(this.setResults);
-
-  //     console.log(this.query);
-  //   },
-  //   setResults(results) {
-  //     this.results = results;
-  //   },
-  // },
 });
